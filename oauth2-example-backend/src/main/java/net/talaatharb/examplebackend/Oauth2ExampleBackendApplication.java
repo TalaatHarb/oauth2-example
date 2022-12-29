39ff78fc77bc152974f4b7c9fd544c88408209c7
Mohamed Talaat Harb <mohamed.harb@cegedim.com>
2022-12-29 16:58:40 +0200

    added empty backend application

---
+++ oauth2-example-backend/src/main/java/net/talaatharb/examplebackend/Oauth2ExampleBackendApplication.java
@@ -0,0 +1,13 @@
+package net.talaatharb.examplebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2ExampleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2ExampleBackendApplication.class, args);
+	}
+
+}
---
+++ oauth2-example-backend/src/test/java/net/talaatharb/examplebackend/Oauth2ExampleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.talaatharb.examplebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2ExampleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
