c798e60b53021fca57646e2b54e755aee453b56c
Mohamed Talaat Harb <mohamed.harb@cegedim.com>
2022-12-31 22:14:04 +0200

    added OpenApi documentation

--- oauth2-example-backend/src/test/java/net/talaatharb/examplebackend/Oauth2ExampleBackendApplicationIT.java
+++ oauth2-example-backend/src/test/java/net/talaatharb/examplebackend/Oauth2ExampleBackendApplicationIT.java
@@ -1,6 +1,7 @@
 package net.talaatharb.examplebackend;
 
 import org.junit.jupiter.api.Test;
+import org.springdoc.webmvc.api.OpenApiWebMvcResource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
@@ -11,6 +12,15 @@ class Oauth2ExampleBackendApplicationIT extends AbstractControllerIT {
 
 	@Autowired
 	ApplicationContext applicationContext;
+
+	@Autowired
+	private OpenApiWebMvcResource openApiDocs;
+
+	@Test
+	void testSwaggerIsConfigured() {
+		assertNotNull(openApiDocs);
+	}
+
 	@Test
 	void contextLoads() {
 		assertNotNull(applicationContext);
